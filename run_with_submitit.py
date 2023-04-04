bf1a985b6ac86ab8108af89c52245845e053f4e2
mb010 <micah.bowles@postgrad.manchestser.ac.uk>
2023-04-04 16:17:59 +0100

    wckey bugfix
    https://github.com/facebookincubator/submitit/issues/1632

--- run_with_submitit.py
+++ run_with_submitit.py
@@ -132,6 +132,7 @@ def main():
         timeout_min=timeout_min,  # max is 60 * 72
         # Below are cluster dependent parameters
         slurm_signal_delay_s=120,
+        wckey="",
         **kwargs,
     )
 
