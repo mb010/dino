1518ab12e6873fbbbe0dcadf52a29199c28f313e
mb010 <micah.bowles@postgrad.manchestser.ac.uk>
2023-04-05 15:34:03 +0100

    cpu allocation

--- run_with_submitit.py
+++ run_with_submitit.py
@@ -126,7 +126,7 @@ def main():
     executor.update_parameters(
         mem_gb=30 * num_gpus_per_node,
         tasks_per_node=num_gpus_per_node,  # one task per GPU
-        cpus_per_task=16,
+        cpus_per_task=8,
         nodes=nodes,
         timeout_min=timeout_min,  # max is 60 * 72
         # Below are cluster dependent parameters
